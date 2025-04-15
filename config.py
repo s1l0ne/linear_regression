cff75e5ee618c25958dd8be1238539c1aef5c293
Timofey <tadjik333@yandex.ru>
2025-04-16 00:08:58 +0300

    refactor, add creating charts

---
+++ config.py
@@ -0,0 +1,24 @@
+from enum import IntEnum
+
+
+class DataSet(IntEnum):
+    house_prices = 0
+
+
+DATASET_CONFIG = {
+    DataSet.house_prices: {
+        'paths': (
+            'house_prices/train.csv',
+            'house_prices/test.csv',
+            'house_prices/sample_submission.csv'
+        ),
+        'features': ['GrLivArea', 'GarageCars', 'FullBath'],
+        'target': 'SalePrice'
+    }
+}
+
+
+def get_config(dataset: DataSet) -> dict:
+    if dataset not in DATASET_CONFIG:
+        raise ValueError(f"Конфигурация для {dataset} не найдена.")
+    return DATASET_CONFIG[dataset]
